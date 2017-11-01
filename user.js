91f86b66634dcb50a761264924728f343ba94a36
Gabriel Ribeiro <biel.ribeiro1990@gmail.com>
2017-11-01 00:37:48 -0200

    remove unnecessary abbreviation

--- user.js
+++ user.js
@@ -1,6 +1,6 @@
 const user = await fetch('http://localhost:8080/user/99');
-const PRCHSES_TO_WIN_PRZE = 1000;
+const PURCHASES_TO_WIN_PRIZE = 1000;
 
-if(user.purchases === PRCHSES_TO_WIN_PRZE) {
+if(user.purchases === PURCHASES_TO_WIN_PRIZE) {
   // GIVE A PRIZE
 }
