ad34215759164515a730946ee34d8bf2b65873b8
Gabriel Ribeiro <biel.ribeiro1990@gmail.com>
2017-11-01 00:31:14 -0200

    add searchable constant to purchases to win a prize

--- user.js
+++ user.js
@@ -1,5 +1,6 @@
 const user = await fetch('http://localhost:8080/user/99');
+const PRCHSES_TO_WIN_PRZE = 1000;
 
-if(user.purchases === 1000) {
+if(user.purchases === PRCHSES_TO_WIN_PRZE) {
   // GIVE A PRIZE
 }
